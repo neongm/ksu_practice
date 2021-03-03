31ff2114641dde0d8b39701ac7f46722e5093d72
neongm <chura2013c@gmail.com>
2021-03-03 16:15:33 +0500

    dab

--- web_ksu_dev/error_test.cpp
+++ web_ksu_dev/error_test.cpp
@@ -5,6 +5,6 @@ int main()
     std::cout<<"Lorem Ipsum doka 2"<<std::endl;
     size_t a = 100000;
     size_t b = 1000;
-    if (a != b): return 100;
+    if (a != b): return 1000000000000000000000000;
     return 0;
 }
