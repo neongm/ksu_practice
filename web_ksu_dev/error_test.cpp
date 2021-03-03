2d5f9d70e22ef06bfb617f5c14c6c2d578c9afa0
neongm <chura2013c@gmail.com>
2021-03-03 16:08:07 +0500

    broke again

--- web_ksu_dev/error_test.cpp
+++ web_ksu_dev/error_test.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    std::cout<<"Lorem Ipsum"<<std::endl;
-    size_t a = 0;
-    size_t b = 0;
-    if (a == b): return 1;
+    std::cout<<"Lorem Ipsum doka 2"<<std::endl;
+    size_t a = 100000;
+    size_t b = 1000;
+    if (a != b): return 100;
     return 0;
 }
