94421887674acaebeb1e123f312157b74550186a
neongm <chura2013c@gmail.com>
2021-03-03 16:19:08 +0500

    slomaysya

--- web_ksu_dev/error_test.cpp
+++ web_ksu_dev/error_test.cpp
@@ -3,8 +3,8 @@
 int main()
 {
     std::cout<<"Lorem Ipsum doka 2"<<std::endl;
-    size_t a = 100000;
-    size_t b = 1000;
-    if (a != b): return 100;
-    return 0;
+    size_t a = 1;
+    size_t b = 1;
+    if (a != b): return 0;
+    return 1;
 }
